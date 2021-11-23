51d37952a19fa847ff9b5e1d609ca352ecb2da5e
Connell Boyce <connell.p.boyce@gmail.com>
2021-11-22 21:46:19 -0500

    Initial commit

---
+++ src/main/java/com/connell/pokemonworldplanner/PokemonworldplannerApplication.java
@@ -0,0 +1,13 @@
+package com.connell.pokemonworldplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonworldplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonworldplannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/connell/pokemonworldplanner/PokemonworldplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connell.pokemonworldplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonworldplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
